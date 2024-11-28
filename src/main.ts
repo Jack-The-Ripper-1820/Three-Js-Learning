9ee9838da00c22ea9d3d5a6e627d271f57945fa3
Jack-The-Ripper-1820 <mayurjreddy@gmail.com>
2024-11-28 17:08:56 -0600

    animation loop

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: false })
-// renderer.setSize(200, 200)
-// const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-// document.body.appendChild(renderer.domElement)
+document.body.appendChild(renderer.domElement)
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
@@ -28,10 +26,23 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const clock = new THREE.Clock()
+let delta
+
 function animate() {
-  requestAnimationFrame(animate)
+  requestAnimationFrame(animate) // will queue the function for the another call in next frame
+
+  delta = clock.getDelta()
+
+  cube.rotation.x += delta
+  cube.rotation.y += delta
 
   renderer.render(scene, camera)
+
+  stats.update()
 }
 
 animate()
