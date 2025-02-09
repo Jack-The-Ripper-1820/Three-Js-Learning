d88aaec8ebe9fd04c6bf36a0f9a69e0940f93c3e
Jack-The-Ripper-1820 <mayurjreddy@gmail.com>
2025-02-09 09:13:35 -0600

    deploy build + gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
-  assetsInclude: ['**/*.hdr', '**/*.glb'],
+  plugins: [topLevelAwait()],
+  base: './',
 })
